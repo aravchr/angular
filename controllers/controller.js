1405098cfa1291d74a6c56f99a0984c6447779fd
Shamis Shukoor <shamis657@gmail.com>
2015-03-26 04:37:22 -0700

    Add/Remove Cart

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,4 +4,23 @@ productController.controller('productListCtrl', ['$scope', '$http',function($sco
 	$http.get('products.json').success(function(data) {
 		$scope.products = data;
 	});
+
+	//cart
+	$scope.cartCount = 0;
+	$scope.cartItems = [];
+	$scope.showModal = false;
+	$scope.addToCart = function(product){
+		$scope.cartCount++;
+		product.cart_id = $scope.cartCount;
+		$scope.cartItems.push(product);
+	};
+	$scope.removeFromCart = function(index){
+		console.log(index);
+		if($scope.cartCount > 0){
+			
+			$scope.cartCount--;
+			$scope.cartItems.splice(index,1);
+		}
+	};
 }]);
+
